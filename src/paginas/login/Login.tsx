9f05d5d5ec41f099635a9d168beff0714aa35fc8
Letícia Queiroga <letqueirogaa@outlook.com>
2023-05-18 08:21:42 -0300

    [Feature Leticia - Task : Link | 18.05.2023]

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Typography, Grid, Button, Paper } from '@material-ui/core';
 import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function Home() {
-    return (
-        <>
-          <a href="https://github.com/Letixs/blogPessoal-frontend">Login</a>
-        </>
-    );
+  return (
+    <>
+      <Link to="/home" className='link'>
+        <h1>Home</h1>
+      </Link>
+      <h1>Login</h1>
+    </>
+  );
 }
 
 export default Home;
