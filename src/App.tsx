974c68dc6c826ea3c82548c121299366957c8258
Letícia Queiroga <letqueirogaa@outlook.com>
2023-05-17 21:27:42 -0300

    [Feature Leticia - Task : NavBar | 17.05.2023]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Navbar from './components/static/navbar/Navbar';
 import Home from './paginas/home/Home';
-import Footer from './components/static/footer/Footer';
+import Footer from './components/static/footer/Footer'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-   <>
-      <Navbar />
-      <Home />
-      <Footer />
-   </>
+    <>
+        <Navbar />
+        <Home />
+        <Footer />
+    </>
   );
 }
 
--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,41 +1,85 @@
-import React from 'react';
+import react from 'react';
+import { Button, Grid, Paper, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
-import './Footer.css'
-import { GitHub } from '@material-ui/icons';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+
+import './Footer.css';
+import { GitHub } from '@mui/icons-material';
 
 function Footer() {
+
     return (
-        <>
-            <Grid className='footer' container direction="row" justifyContent="center" alignItems="center" style={{position:"fixed", bottom: 0, left: 0, right: 0}}>
-                <Grid alignItems="center" item xs={12}>
-                    <Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://github.com/Letixs" target='blank'>
-                            <GitHub style={{ fontSize: 50, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Letícia Queiroga</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+
+        <></>
+
+        // <>
+
+        //     <Grid style={{ backgroundColor: "#ffffff", width: "100%", margin: 0 }} spacing={3} container direction="row" justifyContent="center">
+
+        //         {/* Sobre o projeto */}
+        //         <Grid item xs={5}>
+        //             <Box>
+        //                 <h4 className='titulos'>SOBRE O PROJETO</h4>
+        //                 <p className="texto">
+        //                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus doloremque dolorem ratione itaque impedit ipsam qui veniam consequuntur. Laboriosam dignissimos totam provident dolorum suscipit quasi aspernatur voluptatem tempora ex. </p>
+        //             </Box>
+        //         </Grid>
+
+        //         {/* lista de tecnologias */}
+        //         <Grid item xs={3}>
+        //             <Box>
+        //                 <h4 className='titulos'>TECNOLOGIAS UTILIZADAS</h4>
+        //                 <p className="texto">
+        //                     Java
+        //                 </p>
+        //                 <p className="texto">
+        //                     Java
+        //                 </p>
+        //                 <p className="texto">
+        //                     Java
+        //                 </p>
+        //                 <p className="texto">
+        //                     Java
+        //                 </p>
+        //                 <p className="texto">
+        //                     Java
+        //                 </p>
+        //             </Box>
+        //         </Grid>
+
+        //         {/* redes */}
+        //         <Grid item xs={3} >
+        //             <Box style={{ marginRight: "0px" }}>
+        //                 <h4 className='titulos'>REDES</h4>
+        //                 <Box className='redes' style={{ display: "flex" }}>
+        //                     <p className='texto1'>Siga-nos</p>
+        //                     <Box className='redesIcon'>
+        //                         <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+        //                             <LinkedInIcon style={{ fontSize: 49, color: "537a56" }} />
+        //                         </a>
+        //                         <a className='iconGit' href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+        //                             <GitHubIcon style={{ fontSize: 45, color: "537a56" }} />
+        //                         </a>
+        //                     </Box>
+        //                 </Box>
+        //             </Box>
+        //         </Grid>
+
+        //         {/* copyright */}
+        //         <Grid item xs={11} style={{ marginBottom: "-7px" }}>
+        //             <hr></hr>
+        //             <Box style={{ marginLeft: "60rem", paddingTop: "4px" }}>
+        //                 <Typography variant="subtitle2" align="center" style={{ color: "black" }}>© 2023 Copyright: SaúdeCheckup</Typography>
+        //             </Box>
+        //         </Grid>
+        //     </Grid>
+
+        // </>
+        )
 }
 
 export default Footer;
